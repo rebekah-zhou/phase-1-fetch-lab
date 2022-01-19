91fc9b0f23a7069da5d211e31158c209789df95d
Rebekah Zhou <rebekahzhou@gmail.com>
2022-01-18 16:03:36 -0800

    commit tree c02918fd5abb67505c673eb11279223c117fcde0

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((json) => {
+    renderBooks(json)
+    totalPages(json)
+  });
+}
+
+function totalPages(books) {
+  let totalNumOfPages = 0
+  books.forEach(book => {
+    totalNumOfPages += book.numberOfPages
+  })
+  return totalNumOfPages
 }
 
 function renderBooks(books) {
